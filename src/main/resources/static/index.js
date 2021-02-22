263f02d356d8e1a0da08200bd8e35ec0a9095b61
Vyaceslav Sv <nr5wqdncrvuq@mail.ru>
2021-02-22 11:43:15 +0300

    js edit

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -57,61 +57,6 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
 
     };
 
-    $scope.editFiles = function () {
-        var f = document.getElementById('file').files[0],
-            r = new FileReader();
-
-        r.onloadend = function (e) {
-            var data = e.target.result;
-            //send your binary data via $http or $resource or do anything else with it
-
-            $http.post(contextPath + '/files_lessons')
-                .then(function (response) {
-                    $scope.fillTableFiles();
-                });
-        }
-        r.readAsArrayBuffer(f);
-    }
-
-
-    //
-    // $scope.editLesson = function (id,title,description) {
-    //     var data = {
-    //         id: id,
-    //         title: title,
-    //         description: description
-    //     };
-    //
-    //     $http.put(contextPath + '/lessons/', JSON.stringify(data))
-    //         .then(function (response) {
-    //             $scope.msg = "Put Data Method Executed Successfully!";
-    //             $scope.fillTableLesson();
-    //         });
-    //
-    //     $http({
-    //         method : "PUT",
-    //         url : "employees/" + $scope.form.id,
-    //         data : angular.toJson($scope.form),
-    //         headers : {
-    //             'Content-Type' : 'application/json'
-    //         }
-    //     }).then( _success, _error );
-    //
-    //
-    //     // $http({
-    //     //     url: contextPath + '/lessons',$scopenewLesson,
-    //     //     method: 'POST',
-    //     //     params: {
-    //     //         id: $scope.lesson.id
-    //     //     }}).then(function (response) {
-    //     //     window.alert("OK!");
-    //     // });
-    //
-    //
-    // };
-    //
-
     $scope.fillTableLesson();
     $scope.fillTableFiles();
-
 })
